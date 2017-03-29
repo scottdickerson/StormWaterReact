a2eddead379f5e828ae203b58dd1b6bcbdb6311d
Scott Dickerson <swiftfoot@hotmail.com>
2017-03-29 17:28:45 -0500

    Gardens video
    Gardens video

--- src/CenterButton.js
+++ src/CenterButton.js
@@ -5,17 +5,50 @@ class CenterButton extends Component {
 
     constructor(props) {
         super(props);
+        let horizontalOffset = "425px";
+        let verticalOffset = "-60px";
+        switch (this.props.position) {
+            case "top":
+                this.style= {
+                    left: horizontalOffset,
+                    top: verticalOffset,
+                }
+                break;
+            case "bottom":
+                this.style= {
+                    left: horizontalOffset,
+                    bottom: verticalOffset,
+                }
+                break;
+            case "left":
+                this.style= {
+                    left: verticalOffset,
+                    bottom: horizontalOffset,
+                }
+                break;
+            case "right":
+                this.style= {
+                    right: verticalOffset,
+                    bottom: horizontalOffset,
+                }
+                break;
+            default:
+                this.style= {
+                    left: horizontalOffset,
+                    top: verticalOffset,
+                }
+        }
     }
 
     render() {
 
         return (
-            <div className="buttonOuterCircle">
-                <div className="centerButton" onClick={this.props.onClick.bind(this)}>
-                    <div className="label">
-                        {this.props.label}
-                    </div>
+            <div className="buttonOuterCircle" style={this.style}>
+                <div className="centerButton" onClick={this.props.onClick.bind(this)} >
+                <div className="label">
+                    {this.props.label}
                 </div>
+            </div>
             </div>
         );
 
--- src/CenterDetail.js
+++ src/CenterDetail.js
@@ -5,17 +5,25 @@ import CenterButton from './CenterButton';
 
 class CenterDetail extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {video: "videos/greenRoof.mp4"};
+    }
+
     render() {
 
         return (
             <div className="outerCircle">
 
                 <video id="centerVideo" className="centerVideo" preload="true">
-                    <source src="videos/test.mp4" type="video/mp4" />
+                    <source src={this.state.video} type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
 
-                <CenterButton label="Green Roof" onClick={this.greenRoof.bind(this)}></CenterButton>
+                <CenterButton label="Green Roof" onClick={this.greenRoof.bind(this)} position="top"></CenterButton>
+                <CenterButton label="Rain Gardens" onClick={this.rainGardens.bind(this)} position="right"></CenterButton>
+                <CenterButton label="Permeable Pavement" onClick={this.permeablePavement.bind(this)} position="bottom"></CenterButton>
+                <CenterButton label="Landscaping" onClick={this.landScaping.bind(this)} position="left"></CenterButton>
             </div>
         );
 
@@ -23,21 +31,31 @@ class CenterDetail extends Component {
 
     greenRoof() {
         console.log("Green Roof clicked");
-        var video = document.getElementById("centerVideo");
-        video.load();
-        video.play();
+        this.loadVideo("videos/greenRoof.mp4");
     }
 
     rainGardens() {
-
+        console.log("Rain Gardens clicked");
+        this.loadVideo("videos/rainGardens.mp4");
     }
 
     permeablePavement() {
-
+        console.log("Permeable Pavement clicked");
+        this.loadVideo("videos/permeablePavement.mp4");
     }
 
     landScaping() {
+        console.log("Landscaping clicked");
+        this.loadVideo("videos/landscaping.mp4");
+    }
 
+    loadVideo(videoName) {
+        this.setState( {
+            video: videoName,
+        })
+        var video = document.getElementById("centerVideo");
+        video.load();
+        video.play();
     }
 }
 
