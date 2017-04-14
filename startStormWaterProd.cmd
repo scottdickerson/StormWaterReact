dd506d9da8b537a40089679c2da4aa7ba752dfe7
Scott Dickerson <swiftfoot@hotmail.com>
2017-04-14 08:29:09 -0500

    fix to final built index
    fix to final built index

--- startStormWaterProd.cmd
+++ startStormWaterProd.cmd
@@ -1,1 +1,3 @@
+taskkill /IM chrome.exe >nul
+taskkill /f /im explorer.exe
 start chrome --kiosk --app=file:///C:/Users/admin/Documents/GitHub/StormWaterReact/build/index.html --start-fullscreen --disable-pinch
